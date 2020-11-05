b77402bcc009146ad1a349e4533a04278d1103b4
jankubatt <jankubat.1@seznam.cz>
2020-11-05 16:23:55 +0100

    Decluttering, new game logic, previous didn't work

--- Snake/run_snake.py
+++ Snake/run_snake.py
@@ -7,28 +7,22 @@
 
 
 import pygame as pg
-import random
 
-random.seed()
 pg.init()
 clock = pg.time.Clock()
 pg.display.set_caption('Snake') 
 size = 800
 screen = pg.display.set_mode((size, size))
-x = 0
-y = 0
+x = size/2
+y = size/2
 direction = 0
 done = False
-eaten = False
-generated = False
-
-def is_collided_with(self, sprite):
-    return self.rect.colliderect(sprite.rect)
 
 while not done:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                         done = True
+                        
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_w and direction != 1:
                     direction = 3
@@ -40,17 +34,16 @@ while not done:
                     direction = 0
         
         if (direction == 0):
-            x+=5
+            x+=25
 
         if (direction == 1):
-            y+=5
+            y+=25
 
         if (direction == 2):
-            x-=5
+            x-=25
 
         if (direction == 3):
-            y-=5
-
+            y-=25
 
         if x < 0:
             x = size
@@ -65,26 +58,6 @@ while not done:
             y = 0
             
         screen.fill((0,0,0))
-
-        if eaten == False:
-            if generated == False:
-                xFood = random.randint(10, size - 10)
-                yFood = random.randint(10, size - 10)
-                generated = True
-        else:
-            generated = False
-            eaten = False
-        
-        food = pg.draw.rect(screen, (255,0,0), (xFood, yFood, 20, 20))
-        player = pg.draw.rect(screen, (255,255,255), (x, y, 25, 25))
-
-        
-
-        pg.draw.rect(screen, (255,0,0), (xFood, yFood, 20, 20))
-        pg.draw.rect(screen, (255,255,255), (x, y, 25, 25))
-
-        if player.colliderect(food):
-            eaten = True
-
+        pg.draw.rect(screen, (255, 255, 255), (x, y, 25, 25))
         pg.display.update()
-        clock.tick(60)
+        clock.tick(10)
